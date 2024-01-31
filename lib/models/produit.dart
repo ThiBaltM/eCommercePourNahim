1b0f73b1cef5dc2dcc5f3a8bc9cb9dce05906720
ThiBaltM <thibalt.meurin@gmail.com>
2024-01-31 14:25:37 +0100

    first commit

---
+++ lib/models/produit.dart
@@ -0,0 +1,9 @@
+class Produit{
+  int id;
+  String title;
+  String image;
+  double price;
+  String size;
+  String collection;
+  Produit(this.id, this.title, this.image, this.price, this.size, this.collection);
+}
